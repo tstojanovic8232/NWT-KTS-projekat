fe25e98e533f8d6845c9c563193c2d4cc8fcb9da
IgorPavlov123 <igorpavlov106@gmail.com>
2023-02-28 21:44:10 +0100

    ispravljen bug sa mapom

--- AngularClient/src/app/map/map.component.ts
+++ AngularClient/src/app/map/map.component.ts
@@ -20,7 +20,11 @@ export class MapComponent implements OnInit {
   private options: any;
   private markers: L.Marker[] = [];
   private markers2: L.Marker[] = [];
-
+  private fromMarker: L.Marker;
+  private toMarker: L.Marker;
+  // @ts-ignore
+  private routingControl: L.Routing.Control;
+  private tiles: L.TileLayer;
   private initMap(): void {
     this.map = L.map('map', {
       center: this.centroid,
@@ -54,11 +58,9 @@ export class MapComponent implements OnInit {
 
 
   updateMapFrom() {
-    this.markers2 = this.markers2.filter(marker => {
-      marker.remove();
-
-      return false;
-    });
+    if (this.fromMarker) {
+      this.map.removeLayer(this.fromMarker);
+    }
 
     const url = `https://nominatim.openstreetmap.org/search?q=${this.addressFrom}&format=json`;
     this.http.get(url).subscribe(data => {
@@ -66,85 +68,48 @@ export class MapComponent implements OnInit {
       const coordinates = data[0];
       this.setFrom(coordinates);
       this.map.setView(new L.LatLng(coordinates.lat, coordinates.lon), 18);
-      const marker = L.marker([coordinates.lat, coordinates.lon], {title: this.addressFrom}).addTo(this.map);
-      this.markers2.push(marker);
-      // check if both addresses have been set
-      if (this.markers2.length == 1 && this.markers.length==1) {
-        // create routing control
+      this.fromMarker = L.marker([coordinates.lat, coordinates.lon], { title: this.addressFrom }).addTo(this.map);
 
-        var Routing: any = require('leaflet-routing-machine');
-        let control: any;
-        // @ts-ignore
-        control = L.Routing.control({
-
-          draggableWaypoints:false,
-          waypoints: [
-            L.latLng(this.markers2[0].getLatLng().lat, this.markers2[0].getLatLng().lng),
-            L.latLng(this.markers[0].getLatLng().lat, this.markers[0].getLatLng().lng)
-          ]
-        });
-        console.log(this.markers2);
-        console.log(this.markers);
-        this.markers2 = this.markers2.filter(marker => {
-          marker.remove();
-
-          return false;
-        });
-
-
-        // add routing control to the map
-        control.addTo(this.map);
+      if (this.toMarker) {
+        this.addRoutingControl();
       }
     });
   }
 
   updateMapTo() {
-    this.markers = this.markers.filter(marker => {
-      marker.remove();
+    if (this.toMarker) {
+      this.map.removeLayer(this.toMarker);
+    }
 
-      return false;
-    });
-    this.markers = [];
     const url = `https://nominatim.openstreetmap.org/search?q=${this.addressTo}&format=json`;
     this.http.get(url).subscribe(data => {
       // @ts-ignore
       const coordinates = data[0];
       this.setTo(coordinates);
-
       this.map.setView(new L.LatLng(coordinates.lat, coordinates.lon), 18);
-      const marker = L.marker([coordinates.lat, coordinates.lon], {title: this.addressTo}).addTo(this.map);
-      this.markers.push(marker);
+      this.toMarker = L.marker([coordinates.lat, coordinates.lon], { title: this.addressTo }).addTo(this.map);
 
-
-      if (this.markers2.length == 1 && this.markers.length==1) {
-        // create routing control
-
-        var Routing: any = require('leaflet-routing-machine');
-        let control: any;
-        // @ts-ignore
-
-        control = L.Routing.control({
-
-          draggableWaypoints:false,
-
-          waypoints: [
-            L.latLng(this.markers2[0].getLatLng().lat, this.markers2[0].getLatLng().lng),
-            L.latLng(this.markers[0].getLatLng().lat, this.markers[0].getLatLng().lng)
-          ]
-
-
-        });
-
-
-        console.log(this.markers2);
-
-        console.log(this.markers);
-
-        // add routing control to the map
-        control.addTo(this.map);
+      if (this.fromMarker) {
+        this.addRoutingControl();
       }
-
     });
+  }
+
+  addRoutingControl() {
+    if (this.routingControl) {
+      this.map.removeControl(this.routingControl);
+    }
+
+    // @ts-ignore
+    this.routingControl = L.Routing.control({
+      draggableWaypoints: false,
+      waypoints: [
+        this.fromMarker.getLatLng(),
+        this.toMarker.getLatLng()
+      ]
+    });
+
+    this.routingControl.addTo(this.map);
   }
   setFrom(data:any){
     let str:string|number=data.lat+", "+data.lon;
