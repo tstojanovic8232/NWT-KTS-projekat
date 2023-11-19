24c2a3ed673fd3e55e1d7de0edde389f3ec3f15e
IgorPavlov123 <igorpavlov106@gmail.com>
2023-11-19 15:35:28 +0100

    dodata jos 4 testa za korisnikServis

--- projekat/src/test/java/tim/projekat/unit/service/KorisnikServisTests.java
+++ projekat/src/test/java/tim/projekat/unit/service/KorisnikServisTests.java
@@ -17,10 +17,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import tim.projekat.model.Klijent;
-import tim.projekat.model.Korisnik;
-import tim.projekat.model.VerificationToken;
-import tim.projekat.model.Vozac;
+import tim.projekat.model.*;
 import tim.projekat.repozitorijumi.KorisnikRepo;
 import tim.projekat.repozitorijumi.TokenRepo;
 import tim.projekat.servisi.KorisnikServis;
@@ -66,8 +63,72 @@ public class KorisnikServisTests {
         Assertions.assertEquals(1, result.size()); // Assuming only one user of the specified type in the list
         Assertions.assertTrue(result.get(0) instanceof Klijent);
     }
+    @Test
+    void findAllByTypeShouldReturnOnlyVozac() {
+        String userType = "Vozac";
+        List<Korisnik> userList = Arrays.asList(new Klijent(), new Vozac()); // Replace with actual user initialization
 
+        Mockito.when(korisnikRepo.findAll()).thenReturn(userList);
+
+        List<Korisnik> result = korisnikServis.findAllByType(userType);
+
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertTrue(result.get(0) instanceof Vozac);
+    }
+
+    @Test
+    void findAllByTypeShouldReturnEmptyList() {
+        String userType = "NonExistentType";
+        List<Korisnik> userList = Arrays.asList(new Klijent(), new Vozac()); // Replace with actual user initialization
+
+        Mockito.when(korisnikRepo.findAll()).thenReturn(userList);
+
+        List<Korisnik> result = korisnikServis.findAllByType(userType);
+
+        Assertions.assertTrue(result.isEmpty());
+    }
     // Add more test cases for other methods as needed
+
+
+    @Test
+    void getKlijentShouldReturnKlijentForVoznja() {
+        // Arrange
+        Voznja voznja = new Voznja();
+        Klijent expectedKlijent = new Klijent();
+
+        // Use anyLong() or eq() to handle null argument
+        when(korisnikRepo.getKlijentByVoznjeContains(eq(voznja.getId()))).thenReturn(expectedKlijent);
+
+        // Act
+        Klijent resultKlijent = korisnikServis.getKlijent(voznja);
+
+        // Assert
+        assertThat(resultKlijent).isNotNull();
+        assertThat(resultKlijent).isSameAs(expectedKlijent);
+
+        // Verify that the repository method was called with the correct argument
+        verify(korisnikRepo, times(1)).getKlijentByVoznjeContains(voznja.getId());
+    }
+
+
+
+    @Test
+    void getKlijentShouldReturnNullForNonExistingVoznja() {
+        // Arrange
+        Voznja nonExistingVoznja = new Voznja();
+
+        // Use anyLong() or eq() to handle null argument
+        when(korisnikRepo.getKlijentByVoznjeContains(eq(nonExistingVoznja.getId()))).thenReturn(null);
+
+        // Act
+        Klijent resultKlijent = korisnikServis.getKlijent(nonExistingVoznja);
+
+        // Assert
+        assertThat(resultKlijent).isNull();
+
+        // Verify that the repository method was called with the correct argument
+        verify(korisnikRepo, times(1)).getKlijentByVoznjeContains(nonExistingVoznja.getId());
+    }
 
 
     @BeforeEach
