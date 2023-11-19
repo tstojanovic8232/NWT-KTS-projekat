1f08866e8af9183eade29661ed56dc8b290f978e
Teodora Stojanovic <t.stojanovic8232@gmail.com>
2023-11-19 14:54:03 +0100

    Zavrseni VoznjaServisTests

--- projekat/src/test/java/tim/projekat/unit/service/VoznjaServisTests.java
+++ projekat/src/test/java/tim/projekat/unit/service/VoznjaServisTests.java
@@ -8,15 +8,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tim.projekat.model.Klijent;
 import tim.projekat.model.Vozac;
 import tim.projekat.model.Voznja;
 import tim.projekat.repozitorijumi.VoznjaRepo;
 import tim.projekat.servisi.VoznjaServis;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,21 +39,26 @@ public class VoznjaServisTests {
     private List<Voznja> buduceVoznje;
 
     private Vozac vozac;
+    private Klijent klijent;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         voz = new Voznja();
         voz.setId(1L);
+        voz.setDatumVreme(LocalDateTime.parse("2023-03-15 08:30:00", formatter));
         voz.setGotova(false);
 
         voz2 = new Voznja();
         voz2.setId(2L);
+        voz2.setDatumVreme(LocalDateTime.parse("2023-03-15 09:30:00", formatter));
         voz2.setGotova(false);
 
         voz3 = new Voznja();
         voz3.setId(3L);
+        voz3.setDatumVreme(LocalDateTime.parse("2023-03-20 08:30:00", formatter));
         voz3.setGotova(true);
 
         vozac = new Vozac();
@@ -57,6 +66,13 @@ public class VoznjaServisTests {
         voznje.add(voz);
         voznje.add(voz3);
         vozac.setVoznje(voznje);
+
+        klijent = new Klijent();
+        List<Voznja> klijentVoznje = new ArrayList<>();
+        klijentVoznje.add(voz);
+        klijentVoznje.add(voz2);
+        klijent.setVoznje(klijentVoznje);
+
 
         buduceVoznje = new ArrayList<>();
     }
@@ -121,4 +137,124 @@ public class VoznjaServisTests {
         assertThat(upcomingVoznje).isNotNull();
         assertThat(upcomingVoznje).isEmpty();
     }
+
+    @Test
+    void givenNullVozac_whenUpcoming_thenThrowNullPointerException() {
+        // Arrange
+        Vozac v = null;
+
+        // Act and Assert
+        assertThrows(NullPointerException.class, () -> vozServis.getDriverUpcoming(v));
+    }
+
+    @Test
+    void givenVozac_thenReturnUpcomingVoznjeSortedByDatumVreme() {
+        // Arrange
+        buduceVoznje.add(voz2);
+        buduceVoznje.add(voz);
+        when(vozRepo.getVoznjasByGotovaFalse()).thenReturn(buduceVoznje);
+        vozac.getVoznje().add(voz2);
+
+        // Act
+        List<Voznja> upcomingVoznje = vozServis.getDriverUpcoming(vozac);
+
+        // Assert
+        assertThat(upcomingVoznje).isNotNull();
+        assertThat(upcomingVoznje).hasSize(2);
+        assertThat(upcomingVoznje).containsExactly(voz, voz2);
+    }
+
+    @Test
+    void givenKlijent_thenReturnClientHistory() {
+        // Arrange
+        List<Voznja> klijentVoznje = new ArrayList<>();
+        klijentVoznje.add(voz);
+        klijentVoznje.add(voz2);
+        klijent.setVoznje(klijentVoznje);
+
+        List<Voznja> gotoveVoznje = new ArrayList<>();
+        gotoveVoznje.add(voz);
+        gotoveVoznje.add(voz2);
+        when(vozRepo.getVoznjasByGotovaTrue()).thenReturn(gotoveVoznje);
+
+        // Act
+        List<Voznja> clientHistory = vozServis.getClientHistory(klijent);
+
+        // Assert
+        assertThat(clientHistory).isNotNull();
+        assertThat(clientHistory).hasSize(2);
+        assertThat(clientHistory).containsExactly(voz2, voz);
+    }
+
+    @Test
+    void givenKlijentWithNoHistory_thenReturnEmptyList() {
+        // Arrange
+        List<Voznja> gotoveVoznje = new ArrayList<>();
+        when(vozRepo.getVoznjasByGotovaTrue()).thenReturn(gotoveVoznje);
+        klijent.setVoznje(new ArrayList<>());
+
+        // Act
+        List<Voznja> clientHistory = vozServis.getClientHistory(klijent);
+
+        // Assert
+        assertThat(clientHistory).isNotNull();
+        assertThat(clientHistory).isEmpty();
+    }
+
+    @Test
+    void givenNullKlijent_thenThrowNullPointerException() {
+        // Arrange
+        Klijent klijent = null;
+
+        // Act and Assert
+        assertThrows(NullPointerException.class, () -> vozServis.getClientHistory(klijent));
+    }
+
+    @Test
+    void givenVozac_thenReturnDriverHistory() {
+        // Arrange
+        List<Voznja> vozacVoznje = new ArrayList<>();
+        vozacVoznje.add(voz);
+        vozacVoznje.add(voz3);
+        vozac.setVoznje(vozacVoznje);
+
+        List<Voznja> gotoveVoznje = new ArrayList<>();
+        gotoveVoznje.add(voz);
+        gotoveVoznje.add(voz3);
+        when(vozRepo.getVoznjasByGotovaTrue()).thenReturn(gotoveVoznje);
+
+        // Act
+        List<Voznja> driverHistory = vozServis.getDriverHistory(vozac);
+
+        // Assert
+        assertThat(driverHistory).isNotNull();
+        assertThat(driverHistory).hasSize(2);
+        assertThat(driverHistory).containsExactly(voz3, voz);
+    }
+
+    @Test
+    void givenVozacWithNoHistory_thenReturnEmptyList() {
+        // Arrange
+        List<Voznja> gotoveVoznje = new ArrayList<>();
+        when(vozRepo.getVoznjasByGotovaTrue()).thenReturn(gotoveVoznje);
+        vozac.setVoznje(new ArrayList<>());
+
+        // Act
+        List<Voznja> driverHistory = vozServis.getDriverHistory(vozac);
+
+        // Assert
+        assertThat(driverHistory).isNotNull();
+        assertThat(driverHistory).isEmpty();
+    }
+
+    @Test
+    void givenNullVozac_whenHistory_thenThrowNullPointerException() {
+        // Arrange
+        Vozac vozac = null;
+
+        // Act and Assert
+        assertThrows(NullPointerException.class, () -> vozServis.getDriverHistory(vozac));
+    }
+
+
 }
