3fb769a4b25f4a17f7fa3e54ef2db362412497ed
IgorPavlov123 <igorpavlov106@gmail.com>
2024-01-06 11:30:17 +0100

    dodata 3 integraciona testa za Voznju Kontroler

--- projekat/src/test/java/tim/projekat/integration/VoznjaKontrolerZakazivanjeTest.java
+++ projekat/src/test/java/tim/projekat/integration/VoznjaKontrolerZakazivanjeTest.java
@@ -4,4 +4,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class VoznjaKontrolerZakazivanjeTest {
+
 }
