f6e0cae88c8b557031ddedad72c68152c557a005
IgorPavlov123 <igorpavlov106@gmail.com>
2024-01-17 15:02:08 +0100

    dodat e2e test za login

--- projekat/src/test/java/tim/projekat/e2e/pages/LoginPage.java
+++ projekat/src/test/java/tim/projekat/e2e/pages/LoginPage.java
@@ -1,4 +1,46 @@
 package tim.projekat.e2e.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+
+    private WebDriver driver;
+
+    // Define locators for the elements on the login page
+    private By emailInputLocator = By.xpath("//input[@placeholder='Email']");
+    private By passwordInputLocator = By.xpath("//input[@placeholder='Password']");
+    private By signInButtonLocator = By.xpath("//button[@type='submit' and contains(@class, 'submit')]");
+
+    // Constructor to initialize the WebDriver
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Method to enter email in the email input field
+    public void enterEmail(String email) {
+        WebElement emailInput = driver.findElement(emailInputLocator);
+        emailInput.sendKeys(email);
+    }
+
+    // Method to enter password in the password input field
+    public void enterPassword(String password) {
+        WebElement passwordInput = driver.findElement(passwordInputLocator);
+        passwordInput.sendKeys(password);
+    }
+
+    // Method to click the Sign In button
+    public void clickSignInButton() {
+        WebElement signInButton = driver.findElement(signInButtonLocator);
+        signInButton.click();
+        System.out.println("kliknuto");
+    }
+
+    // Method to perform login
+    public void login(String email, String password) {
+        enterEmail(email);
+        enterPassword(password);
+        clickSignInButton();
+    }
 }
--- projekat/src/test/java/tim/projekat/e2e/tests/LoginTest.java
+++ projekat/src/test/java/tim/projekat/e2e/tests/LoginTest.java
@@ -1,4 +1,36 @@
 package tim.projekat.e2e.tests;
 
-public class LoginTest extends TestBase{
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import tim.projekat.e2e.pages.LoginPage;
+
+public class LoginTest {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        // Navigate to your login page
+        driver.get("http://localhost:4200/login");
+
+        // Create an instance of LoginPage
+        LoginPage loginPage = new LoginPage(driver);
+
+        // Use the loginPage object to perform login
+        loginPage.login("pera@gmail.com", "222");
+        System.out.println("After login, current URL: " + driver.getCurrentUrl());
+
+        // Wait for some time to see the result (optional)
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Close the browser
+        driver.quit();
+    }
 }
